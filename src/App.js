a0244de92e1052f1c1153a3d6bf9e592ede60842
vlad <vladislav.jorin@gmail.com>
2021-01-28 20:40:39 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+
+import { Route } from "react-router-dom";
+import { Header } from "./components";
+import { Home, Cart } from "./pages/index";
+import { setPizzas } from "./redux/actions/pizzas";
+
+function App() {
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    axios.get("http://localhost:3000/db.json").then(({ data }) => {
+      dispatch(setPizzas(data.pizzas));
+    });
+  });
+
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Route path="/" component={Home} exact />
+        <Route exact path="/Cart" component={Cart} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Categories, SortPopup, PizzaBlock } from "../components";
+function Home() {
+  const { items } = useSelector(({ pizzas }) => {
+    return {
+      items: pizzas.items,
+    };
+  });
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories
+          onClickItem={(name) => {
+            console.log(name);
+          }}
+          items={["Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"]}
+        />
+        <SortPopup
+          items={[
+            { name: "популярности", type: "rating" },
+            { name: "цене", type: "price" },
+            { name: "алфавиту", type: "alphabet" },
+          ]}
+        />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {items && items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
