a7367e7f20f97c766491f0142aafa21670b7b3ce
vlad <vladislav.jorin@gmail.com>
2021-01-29 20:16:51 +0300

    '29'

--- src/App.js
+++ src/App.js
@@ -3,13 +3,8 @@ import React from "react";
 import { Route } from "react-router-dom";
 import { Header } from "./components";
 import { Home, Cart } from "./pages/index";
-import { fetchPizzas } from "./redux/actions/pizzas";
 
 function App() {
-  React.useEffect(() => {
-    fetchPizzas();
-  });
-
   return (
     <div className="wrapper">
       <Header />
